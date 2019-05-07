98cf5bbd36f01ec01517d9aa07a3ca79faec1e41
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-05-07 08:11:52 +0545

    Add package.json and example

---
+++ examples/basic.js
@@ -0,0 +1,21 @@
+const Observer = require('../');
+
+// Create a new observer
+const observer = new Observer();
+
+// A function that is called when the observer gets a new value
+const subscriber = data => console.log('Changed', data);
+
+// Subscribe to the observer, will return a function that will unsubscribe when called.
+const unsubscribe = observer.subscribe(subscriber);
+
+// Change state of the observer
+observer.set(10);
+observer.set(20);
+observer.set(30);
+
+// Unsubscribe from the observer
+unsubscribe();
+
+// Change state again.
+observer.set(40);
--- index.js
+++ index.js
@@ -38,3 +38,5 @@ class Observer {
     return data;
   }
 }
+
+module.exports = Observer;
